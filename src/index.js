6b0d9b6069762a827edef3d72ebae4cce5ef538e
Muhamed Sabic <muhamed.sabic@sap.com>
2021-07-12 16:02:48 +0100

    Redux persist and chrome extension

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from './store.js';
 import App from './App.js';
-
+import {PersistGate} from 'redux-persist/lib/integration/react';
+import {persistStore} from 'redux-persist';
 
 //wrap the app component in the redux provider and pass the store
+const store = configureStore();
+const persistor = persistStore(store);
+
 ReactDOM.render(
-    <Provider store ={configureStore()}>
-        <App />
+    <Provider store ={store}>
+        <PersistGate 
+            loading={<div>loading...</div>}
+            persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>, 
     document.getElementById('root'),
 );
--- src/store.js
+++ src/store.js
@@ -1,12 +1,29 @@
 import {createStore, combineReducers} from 'redux';
 import { todos } from './todos/reducers';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
 
 const reducers = {
     todos,
 };
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    stateReconciler: autoMergeLevel2,
+};
+
 //need use combineRecuers to have a root for all reducers
+//need use persisted reducer to keep info after refresh
 const rootReducer = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 //export configureStore and that will contain out store and all of the reducers inside
-export const configureStore = () => createStore(rootReducer);
+//connecting redux dev tools to chrome
+export const configureStore = () => 
+    createStore(
+        persistedReducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__ (),
+        );
